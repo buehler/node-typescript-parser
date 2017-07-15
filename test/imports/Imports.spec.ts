aadc9ae99ce4b85fb084c032c182311ca15d78e7
Christoph Bühler <christoph.buehler@bluewin.ch>
2017-07-15 13:19:20 +0200

    add test for imports isNew()

---
+++ test/imports/Imports.spec.ts
@@ -0,0 +1,100 @@
+import { DefaultImport, ExternalModuleImport, NamedImport, NamespaceImport, StringImport } from '../../src/imports';
+
+describe('Imports', () => {
+
+    describe('DefaultImport', () => { 
+
+        it('should set isNew() when start is undefined', () => {
+            const imp = new DefaultImport('lib', 'alias', undefined, 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should set isNew() when end is undefined', () => {
+            const imp = new DefaultImport('lib', 'alias', 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should not set isNew() when start and end are defined', () => {
+            const imp = new DefaultImport('lib', 'alias', 12, 1337);
+            expect(imp.isNew).toBeFalsy();
+        });
+
+    });
+
+    describe('ExternalModuleImport', () => { 
+
+        it('should set isNew() when start is undefined', () => {
+            const imp = new ExternalModuleImport('lib', 'alias', undefined, 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should set isNew() when end is undefined', () => {
+            const imp = new ExternalModuleImport('lib', 'alias', 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should not set isNew() when start and end are defined', () => {
+            const imp = new ExternalModuleImport('lib', 'alias', 12, 1337);
+            expect(imp.isNew).toBeFalsy();
+        });
+
+    });
+
+    describe('NamedImport', () => { 
+
+        it('should set isNew() when start is undefined', () => {
+            const imp = new NamedImport('lib', undefined, 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should set isNew() when end is undefined', () => {
+            const imp = new NamedImport('lib', 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should not set isNew() when start and end are defined', () => {
+            const imp = new NamedImport('lib', 12, 1337);
+            expect(imp.isNew).toBeFalsy();
+        });
+
+    });
+
+    describe('NamespaceImport', () => { 
+
+        it('should set isNew() when start is undefined', () => {
+            const imp = new NamespaceImport('lib', 'alias', undefined, 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should set isNew() when end is undefined', () => {
+            const imp = new NamespaceImport('lib', 'alias', 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should not set isNew() when start and end are defined', () => {
+            const imp = new NamespaceImport('lib', 'alias', 12, 1337);
+            expect(imp.isNew).toBeFalsy();
+        });
+
+    });
+
+    describe('StringImport', () => { 
+
+        it('should set isNew() when start is undefined', () => {
+            const imp = new StringImport('lib', undefined, 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should set isNew() when end is undefined', () => {
+            const imp = new StringImport('lib', 1337);
+            expect(imp.isNew).toBeTruthy();
+        });
+        
+        it('should not set isNew() when start and end are defined', () => {
+            const imp = new StringImport('lib', 12, 1337);
+            expect(imp.isNew).toBeFalsy();
+        });
+
+    });
+
+});
