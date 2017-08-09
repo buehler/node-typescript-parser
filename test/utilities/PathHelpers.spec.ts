3714201476a11c3e34d7a2faeed94b5c5ffd0e31
Christoph Bühler <christoph@smartive.ch>
2017-08-09 13:12:32 +0200

    testing the pathhelper

--- test/utilities/PathHelpers.spec.ts
+++ test/utilities/PathHelpers.spec.ts
@@ -1,10 +1,33 @@
-import { normalizeFilename } from '../../src/utilities/PathHelpers';
+import * as os from 'os';
+import { normalizeFilename, normalizePathUri } from '../../src/utilities/PathHelpers';
 
 describe('PathHelpers', () => {
 
     describe('normalizePathUri()', () => {
+        const orig = os.platform;
 
-        // it('should')
+        afterEach(() => {
+            (os as any).platform = orig;
+        });
+
+        it('should remove file:// on unix', () => {
+            (os as any).platform = jest.fn(() => 'unix');
+
+            expect(normalizePathUri('file:///root/file.txt')).toBe('/root/file.txt');
+        });
+
+        it('should remove file:// on mac', () => {
+            (os as any).platform = jest.fn(() => 'darwin');
+
+            expect(normalizePathUri('file:///root/file.txt')).toBe('/root/file.txt');
+        });
+
+        it('should remove file:/// on windows', () => {
+            (os as any).platform = jest.fn(() => 'win32');
+
+            expect(normalizePathUri('file:///C:/root/file.txt')).toBe('C:/root/file.txt');
+            expect(normalizePathUri('file:///C:\root\file.txt')).toBe('C:\root\file.txt');
+        });
 
     });
 
