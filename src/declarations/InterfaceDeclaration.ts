12654364cd5cdba2acf3a957410bf0283c8523b8
Christoph Bühler <buehler@users.noreply.github.com>
2017-10-03 07:58:52 +0200

    fix: correctly implement interface (#30)

--- src/declarations/InterfaceDeclaration.ts
+++ src/declarations/InterfaceDeclaration.ts
@@ -1,16 +1,18 @@
+import { AccessorDeclaration } from './AccessorDeclaration';
 import { ClassLikeDeclaration, ExportableDeclaration, GenericDeclaration } from './Declaration';
 import { MethodDeclaration } from './MethodDeclaration';
 import { PropertyDeclaration } from './PropertyDeclaration';
 
 /**
  * Interface declaration that contains defined properties and methods.
- * 
+ *
  * @export
  * @class InterfaceDeclaration
  * @implements {ExportableDeclaration}
  * @implements {GenericDeclaration}
  */
 export class InterfaceDeclaration implements ClassLikeDeclaration, ExportableDeclaration, GenericDeclaration {
+    public accessors: AccessorDeclaration[];
     public typeParameters: string[] | undefined;
     public properties: PropertyDeclaration[] = [];
     public methods: MethodDeclaration[] = [];
