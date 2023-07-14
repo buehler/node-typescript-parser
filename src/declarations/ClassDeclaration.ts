b94af6ceabab6f3270d97f2c176ea0e8fd0e9b73
Felipe Graeff <felipe.graeff@sap.com>
2023-07-14 17:08:28 -0300

    added extends and implements lists to class and interface

--- src/declarations/ClassDeclaration.ts
+++ src/declarations/ClassDeclaration.ts
@@ -1,6 +1,8 @@
 import { AccessorDeclaration } from './AccessorDeclaration';
 import { ConstructorDeclaration } from './ConstructorDeclaration';
-import { ClassLikeDeclaration, ExportableDeclaration, GenericDeclaration } from './Declaration';
+import { AbstractDeclaration, ClassLikeDeclaration, ExportableDeclaration, GenericDeclaration,
+} from './Declaration';
+import { InterfaceDeclaration } from './InterfaceDeclaration';
 import { MethodDeclaration } from './MethodDeclaration';
 import { PropertyDeclaration } from './PropertyDeclaration';
 
@@ -12,18 +14,27 @@ import { PropertyDeclaration } from './PropertyDeclaration';
  * @implements {ClassLikeDeclaration}
  * @implements {ExportableDeclaration}
  * @implements {GenericDeclaration}
+ * @implements {AbstractDeclaration}
  */
-export class ClassDeclaration implements ClassLikeDeclaration, ExportableDeclaration, GenericDeclaration {
+export class ClassDeclaration
+    implements
+        ClassLikeDeclaration,
+        ExportableDeclaration,
+        GenericDeclaration,
+        AbstractDeclaration
+{
     public ctor: ConstructorDeclaration | undefined;
     public accessors: AccessorDeclaration[] = [];
     public properties: PropertyDeclaration[] = [];
     public methods: MethodDeclaration[] = [];
     public typeParameters: string[] | undefined;
-
+    public isAbstract: boolean = false;
+    public implements: InterfaceDeclaration[] = [];
+    public extends: ClassDeclaration[] = [];
     constructor(
         public name: string,
         public isExported: boolean,
         public start?: number,
         public end?: number,
-    ) { }
+  ) {}
 }
